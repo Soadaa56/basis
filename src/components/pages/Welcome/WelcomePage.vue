e2803c7c7af7d9eefb82e933782df6f9b998fba9
Kevin Duffy <soadhlh@gmail.com>
2025-09-04 14:00:42 -0400

    Chore: organize folder structure

--- src/components/pages/Welcome/WelcomePage.vue
+++ src/components/pages/Welcome/WelcomePage.vue
@@ -12,6 +12,12 @@
       <p>Load file <a href="#">here</a></p>
     </div>
   </section>
+  <footer>
+    <div id="footer-copyright">
+      <p>Created by Kevin Duffy - © 2025</p>
+      <p><a href="https://kevin.duffy.dev">KevinDuffy.dev</a></p>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
@@ -22,5 +28,11 @@
   align-items: center;
   min-height: 75vh;
   text-align: center;
+}
+#footer-copyright {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
