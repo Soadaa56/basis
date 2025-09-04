f2571337241c47a71d4289db1ba9a8c4d212ad16
Kevin Duffy <soadhlh@gmail.com>
2025-09-04 15:50:22 -0400

    Content: credit for 3 font awesome icons
    Only one in use currently, but I'll get good use of the other two.

--- src/components/pages/CreditPage.vue
+++ src/components/pages/CreditPage.vue
@@ -5,6 +5,19 @@
   <div class="font-awesome-credit">
     <p>Credit for all icons (including favicon - tab of your tab)</p>
     <a href="https://fontawesome.com/">Font Awesome</a>
+    <ul>
+      <li>
+        <a href="https://fontawesome.com/icons/cube?f=classic&s=solid">Cube</a>
+      </li>
+      <li>
+        <a href="https://fontawesome.com/icons/bars?f=classic&s=solid">Bars</a>
+      </li>
+      <li>
+        <a href="https://fontawesome.com/icons/window-maximize?f=classic&s=regular"
+          >Window-maximize:
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
