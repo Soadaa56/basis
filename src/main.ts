f527b6d0d8b45060b8cb5b319d31ddf190aeb128
Kevin Duffy <soadhlh@gmail.com>
2025-09-04 14:58:21 -0400

    Chore: fontAwesomeIcons able to be imported into project

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// Custom plugins
+import FontAwesomeIcon from './plugins/fontAwesome'
+
 const app = createApp(App)
+app.component('FaIcon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
---
+++ src/plugins/fontAwesome.ts
@@ -0,0 +1,9 @@
+// import fontawesome core + icon component
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faCube } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCube)
+
+export default FontAwesomeIcon
