934f88c05144b9d2b83d89054dc8b5f20dca756f
Kevin Duffy <soadhlh@gmail.com>
2025-09-04 14:42:39 -0400

    Content: base credit attribution for Font Awesome
    I will also link all indivdual icons used here.

---
+++ src/components/pages/CreditPage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Credit For Free Assets</h1>
+  <div class="font-awesome-credit">
+    <p>Credit for all icons (including favicon - tab of your tab)</p>
+    <a href="https://fontawesome.com/">Font Awesome</a>
+  </div>
+</template>
+
+<style scoped></style>
