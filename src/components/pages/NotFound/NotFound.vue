9125a634ec742c3ca1c4259d0281b00b9bbb8681
Kevin Duffy <soadhlh@gmail.com>
2025-09-06 21:14:51 -0400

    Content: Not found page
    With error 418, that I just learned about, over 404

---
+++ src/components/pages/NotFound/NotFound.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section id="not-found">
+    <div class="header-container">
+      <h1>Error 418 - I'm a teapot</h1>
+      <FaIcon class="basis-icon" :icon="['fas', 'mugSaucer']" size="3x" />
+    </div>
+    <p>Sorry, you somehow loaded a page that doesn't exist!</p>
+    <router-link to="/">Go Back Home</router-link>
+  </section>
+</template>
+
+<style scoped lang="scss">
+#not-found {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+.header-container {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+</style>
