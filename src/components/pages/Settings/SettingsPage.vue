49a7adc880a66ff9639ac3fb5fc55fb851f9a7f1
Kevin Duffy <soadhlh@gmail.com>
2025-09-08 11:32:56 -0400

    chore: move pages to folders expected to have multiple tabs or components

---
+++ src/components/pages/Settings/SettingsPage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import FooterLayout from '@/components/layouts/FooterLayout.vue'
+</script>
+
+<template>
+  <h1>Settings</h1>
+  <footer-layout />
+</template>
+
+<style scoped></style>
