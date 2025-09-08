02362830b42991ce46e24caa08e06432481b8a0c
Kevin Duffy <soadhlh@gmail.com>
2025-09-08 13:48:15 -0400

    feat: add areas for panels to be displayed in village

--- src/components/pages/Village/VillagePage.vue
+++ src/components/pages/Village/VillagePage.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { loadSaveFile } from '@/utils/saveFile'
+import ResourcesPanel from './components/resources/ResourcesPanel.vue'
+import WorkersPanel from './components/workers/WorkersPanel.vue'
+import BuildingsPanel from './components/buildings/BuildingsPanel.vue'
 
 const currentSaveFile = loadSaveFile()
 const villageName = currentSaveFile?.villageName
@@ -7,11 +10,23 @@ const villageName = currentSaveFile?.villageName
 
 <template>
   <h1 class="village-name-header">{{ villageName }}</h1>
-  <p>Test</p>
+  <div class="panel-container">
+    <resources-panel></resources-panel>
+    <workers-panel></workers-panel>
+    <buildings-panel></buildings-panel>
+  </div>
 </template>
 
 <style scoped>
 .village-name-header {
   text-align: center;
+}
+
+.panel-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 1rem;
+  height: 75vh;
 }
 </style>
