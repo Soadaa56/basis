fba64e779acc92209bb419087a1bf0f7507ff9da
Kevin Duffy <soadhlh@gmail.com>
2025-09-07 20:46:20 -0400

    content: header for village page shows village name

--- src/components/pages/Village/VillagePage.vue
+++ src/components/pages/Village/VillagePage.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { loadSaveFile } from '@/utils/saveFile'
+
+const currentSaveFile = loadSaveFile()
+const villageName = currentSaveFile?.villageName
+</script>
 
 <template>
-  <h1>Town Page</h1>
+  <h1 class="village-name-header">{{ villageName }}</h1>
 </template>
 
-<style scoped></style>
+<style scoped>
+.village-name-header {
+  text-align: center;
+}
+</style>
