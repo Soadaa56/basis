cc0c54d52f5a02de656fc62ea46903e5092dbe38
Kevin Duffy <soadhlh@gmail.com>
2025-09-08 15:21:18 -0400

    test: show example of how resources will show on panel
    I will v-for through resources and keep a single base-resource-text tag inside ResourcesPanel

--- src/components/pages/Village/components/resources/ResourcesPanel.vue
+++ src/components/pages/Village/components/resources/ResourcesPanel.vue
@@ -1,7 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BaseResourceText from '@/components/ui/BaseResourceText.vue'
+</script>
 
 <template>
-  <h3>Resources Panel</h3>
+  <div class="resource-panel-container">
+    <h3>Resources Panel</h3>
+    <base-resource-text name="gold" :value="0" :max="100" :income="1"></base-resource-text>
+    <base-resource-text name="wood" :value="0" :max="100" :income="1"></base-resource-text>
+    <base-resource-text name="food" :value="0" :max="100" :income="1"></base-resource-text>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.resource-panel-container {
+  display: flex;
+  flex-direction: column;
+}
+</style>
