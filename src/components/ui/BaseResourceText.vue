a09d8c02d1b0a9ed85121b12a819a994ee19a93d
Kevin Duffy <soadhlh@gmail.com>
2025-09-08 15:19:18 -0400

    chore: split rebaseResourceText stats into 3 spans

--- src/components/ui/BaseResourceText.vue
+++ src/components/ui/BaseResourceText.vue
@@ -9,7 +9,8 @@ defineProps<{
 
 <template>
   <div class="resource-text">
-    <span class="resource">{{ name }}: {{ value.toFixed(1) }}/{{ max }}</span>
+    <span class="resource-name">{{ name }}:</span>
+    <span class="resource-numbers">{{ value.toFixed(1) }}/{{ max }}</span>
     <span class="income">(+{{ income }}/s)</span>
   </div>
 </template>
