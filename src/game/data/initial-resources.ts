27aa0ed59a44b6ab873acfdb78bc0b2e08f07a1d
Kevin Duffy <soadhlh@gmail.com>
2025-09-08 21:04:41 -0400

    feat: init game state resource

---
+++ src/game/data/initial-resources.ts
@@ -0,0 +1,41 @@
+// src/game/data/initialResources.ts
+import type { Resource } from '../models/Resource'
+
+export const initialResources: Resource[] = [
+  {
+    id: 'gold',
+    name: 'Gold',
+    currentAmount: 10,
+    baseStorage: 100,
+    baseStorageModifiers: [],
+    calculatedStorage: 100,
+    baseIncome: 1,
+    baseIncomeModifiers: [],
+    calculatedIncome: 1,
+    lastUpdated: Date.now(),
+  },
+  {
+    id: 'wood',
+    name: 'Wood',
+    currentAmount: 10,
+    baseStorage: 100,
+    baseStorageModifiers: [],
+    calculatedStorage: 100,
+    baseIncome: 1,
+    baseIncomeModifiers: [],
+    calculatedIncome: 1,
+    lastUpdated: Date.now(),
+  },
+  {
+    id: 'food',
+    name: 'Food',
+    currentAmount: 10,
+    baseStorage: 100,
+    baseStorageModifiers: [],
+    calculatedStorage: 100,
+    baseIncome: 1,
+    baseIncomeModifiers: [],
+    calculatedIncome: 1,
+    lastUpdated: Date.now(),
+  },
+]
