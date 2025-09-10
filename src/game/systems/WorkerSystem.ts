bda2488fdb25b5864643ae892dd0af3ee89c2f4d
Kevin Duffy <soadhlh@gmail.com>
2025-09-10 16:25:49 -0400

    feat: begin work on WorkerSystem class

---
+++ src/game/systems/WorkerSystem.ts
@@ -0,0 +1,12 @@
+export class workerSystem {
+  workerCount: number
+  constructor(workerCount: number = 1) {
+    this.workerCount = workerCount
+  }
+
+  increaseWorkerCount(number: number) {
+    for (let i = 0; i < number; i++) {
+      this.workerCount++
+    }
+  }
+}
