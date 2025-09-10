0745757253787dd0dcc662eca9fdd7b81d93a1f4
Kevin Duffy <soadhlh@gmail.com>
2025-09-10 18:31:19 -0400

    feat: buildings model

---
+++ src/game/models/Buildings.ts
@@ -0,0 +1,29 @@
+import type { ResourceCost } from '@/game/models/ResourceCost'
+
+export const BuildingIds = {
+  Farm: 'farm',
+  Hut: 'hut',
+  Market: 'market',
+  LoggingCamp: 'loggingCamp',
+  FishingCamp: 'fishingCamp',
+  StorageShed: 'storageShed',
+  Granary: 'granery',
+  Mine: 'mine',
+  Well: 'well',
+  TownHall: 'townHall',
+  /* I'm unsure if I want both BlackSmith & Workshop
+     perhaps they will work on different type of upgrades */
+  BlackSmith: 'blackSmith',
+  WorkShop: 'workShip',
+} as const
+
+type BuildingId = (typeof BuildingIds)[keyof typeof BuildingIds]
+
+export interface Building {
+  id: BuildingId
+  name: string
+  cost: ResourceCost[]
+  count: number
+  maxCount?: number
+  costMultiplier?: number
+}
