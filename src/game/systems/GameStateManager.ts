4c0f0ada98a660b7d3b192787a69b7d95ef748f7
Kevin Duffy <soadhlh@gmail.com>
2025-09-11 12:12:53 -0400

    chore: add imports for gameStateManager

--- src/game/systems/GameStateManager.ts
+++ src/game/systems/GameStateManager.ts
@@ -1,10 +1,17 @@
+import { newGameFile, saveGameFile, loadSaveFile } from '@/utils/saveFile'
+import { ResourceSystem } from '@/game/systems/ResourceSystem'
+import { BuildingSystem } from '@/game/systems/BuildingSystem'
+
 import type { Resource } from '@/game/models/Resource'
+import type { ResourceCost } from '@/game/models/Costs'
 import type { Building } from '@/game/models/Buildings'
+import type { Magic } from '@/game/models/Magic'
 
 export interface GameState {
   resources: Resource[]
   buildings: Building[]
   workers: number
+  magic: Magic[]
 }
 
 export class GameStateManager {}
