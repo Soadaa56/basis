e2398b1b97e2014c8c4ba41ac65ae658ca55f0b2
Kevin Duffy <soadhlh@gmail.com>
2025-09-11 15:43:59 -0400

    feat: add jobs model

---
+++ src/game/models/jobs.ts
@@ -0,0 +1,18 @@
+import type { Unlockable } from '@/game/models/Unlockable'
+
+export const JobIds = {
+  Farmer: 'farmer',
+  Miner: 'miner',
+  Blacksmith: 'blacksmith',
+  Fisher: 'fisher',
+  Merchant: 'merchant',
+} as const
+
+export type JobId = (typeof JobIds)[keyof typeof JobIds]
+
+export interface Job extends Unlockable {
+  id: JobId
+  name: string
+  totalJobs: number
+  assignedWorkers: number
+}
