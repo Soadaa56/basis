a8f8562b2d8e3ac8c17f83890555021a7ecee90a
Kevin Duffy <soadhlh@gmail.com>
2025-09-10 21:42:04 -0400

    feat: add updateBuildingCost to BuildingSystem

--- src/game/systems/BuildingSystem.ts
+++ src/game/systems/BuildingSystem.ts
@@ -17,10 +17,18 @@ export class BuildingSystem {
     return building ? true : false
   }
 
+  updateBuildingCost(building: Building) {
+    building.cost.forEach((cost) => {
+      if (!building.costMultiplier) return
+      return (cost.amount *= building.costMultiplier)
+    })
+  }
+
   incrementBuilding(buildingId: string) {
     const building = this.getBuilding(buildingId)
     if (building) {
       building.count++
+      this.updateBuildingCost(building)
     } else {
       console.log(`Could not run incrementBuilding: ${buildingId} does not exist`)
     }
