16420ceab8a2ae5155aff5d84a5a1f335db19e77
Kevin Duffy <soadhlh@gmail.com>
2025-09-12 19:42:07 -0400

    minor: comment to not use string for unlocks

--- src/game/data/buildingDefinitions.ts
+++ src/game/data/buildingDefinitions.ts
@@ -7,6 +7,7 @@ export const BuildingTypes = {
   JobProducer: 'jobProducer',
   WorkerProducer: 'workerProducer',
   Unlocker: 'unlocker',
+  Hybrid: 'hybrid',
 } as const
 
 export type BuildingType = (typeof BuildingTypes)[keyof typeof BuildingTypes]
@@ -28,7 +29,16 @@ export type BuildingInfo =
     }
   | {
       type: typeof BuildingTypes.Unlocker
-      unlocks: string
+      unlocks: string // will be changed for type safety
+    }
+  | {
+      type: typeof BuildingTypes.Hybrid
+      resource?: ResourceId
+      rate?: number
+      jobType?: JobId
+      addOpenJobs?: number
+      addWorkers?: number
+      unlocks?: string // will be changed for type safety
     }
 
 export const buildingDefinitions: Partial<Record<BuildingId, BuildingInfo>> = {}
