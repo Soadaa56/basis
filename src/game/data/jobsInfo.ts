98843c1de00ef25d6d3e44dad91f79ed4b6ba0d0
Kevin Duffy <soadhlh@gmail.com>
2025-09-13 19:01:09 -0400

    refactor: track many input or outputs for jobinfo
    Also track unlocked status

--- src/game/data/jobsInfo.ts
+++ src/game/data/jobsInfo.ts
@@ -1,11 +1,18 @@
 import type { ResourceId } from '@/game/models/Resource'
 
-// Not sure if any job will do anything besides produce resources
-export type JobInfo = {
+export interface JobOutput {
   resource: ResourceId
   rate: number
-  consume?: {
-    resource: ResourceId
-    amount: number
-  }
+}
+// Still unsure if I should name input 'rate' instead of amount
+export interface JobInput {
+  resource: ResourceId
+  amount: number
+}
+
+// Not sure if any job will do anything besides produce resources
+export type JobInfo = {
+  unlocked: boolean
+  output: JobOutput[]
+  input?: JobInput[]
 }
