a0054744c72432ac5b6e55d083ba446f6526aabd
Kevin Duffy <soadhlh@gmail.com>
2025-09-13 22:14:26 -0400

    feat: add first case for triggerBuilding method
    for use in purchaseBuilding in GameStateManager

--- src/game/systems/BuildingSystem.ts
+++ src/game/systems/BuildingSystem.ts
@@ -1,4 +1,8 @@
-import type { Building } from '@/game/models/Buildings'
+import { buildingDefinitions } from '@/game/data/buildings'
+import { BuildingTypes } from '@/game/data/buildingsInfo'
+
+import type { Building, BuildingId } from '@/game/models/Buildings'
+import type { ResourceSystem } from './ResourceSystem'
 
 export class BuildingSystem {
   private buildings: Building[] = []
@@ -30,6 +34,43 @@ export class BuildingSystem {
       this.updateBuildingCost(building)
     } else {
       console.log(`Could not run incrementBuilding: ${buildingId} does not exist`)
+    }
+  }
+
+  triggerBuilding(buildingId: BuildingId, resourceSystem: ResourceSystem) {
+    const BuildingInfo = buildingDefinitions[buildingId]
+    if (!BuildingInfo) return console.log(`triggerBuilding failed. no BuildingInfo ${BuildingInfo}`)
+    switch (BuildingInfo?.type) {
+      case BuildingTypes.ResourceProducer:
+        break
+
+      case BuildingTypes.ResourceMultiplier:
+        const resourceId = BuildingInfo.resource
+        const multiplier = BuildingInfo.multiplier
+        const resource = resourceSystem.getResource(resourceId)
+
+        resource?.baseIncomeModifiers.push(multiplier)
+        if (!resource)
+          return console.log(
+            `triggerBuilding => case: ResourceMultipler problem with !resource ${resource}`,
+          )
+        resourceSystem.updateCalculatedIncome(resource)
+        break
+
+      case BuildingTypes.JobProducer:
+        break
+
+      case BuildingTypes.WorkerProducer:
+        break
+
+      case BuildingTypes.Unlocker:
+        break
+
+      case BuildingTypes.Hybrid:
+        break
+      default:
+        console.log(`triggerBuilding failed with buildingId: ${buildingId}`)
+        break
     }
   }
 }
