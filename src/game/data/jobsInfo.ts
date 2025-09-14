2c88d7d74a36daf27127deee8ecc01ae136f80be
Kevin Duffy <soadhlh@gmail.com>
2025-09-13 20:26:40 -0400

    feat: add second union option for JobInfo

--- src/game/data/jobsInfo.ts
+++ src/game/data/jobsInfo.ts
@@ -1,18 +1,35 @@
-import type { ResourceId } from '@/game/models/Resource'
+import { type ResourceId } from '@/game/models/Resource'
 
 export interface JobOutput {
   resource: ResourceId
   rate: number
 }
-// Still unsure if I should name input 'rate' instead of amount
+
 export interface JobInput {
   resource: ResourceId
-  amount: number
+  rate: number
 }
 
 // Not sure if any job will do anything besides produce resources
-export type JobInfo = {
-  unlocked: boolean
-  output: JobOutput[]
-  input?: JobInput[]
-}
+export type JobInfo =
+  | {
+      unlocked: boolean
+      output: JobOutput[]
+      input?: JobInput[]
+    }
+  | {
+      unlocked: boolean
+      recipes: [
+        {
+          unlocked: boolean
+          output: JobOutput[]
+          input: JobInput[]
+        },
+      ]
+    }
+
+/* I'm focusing on getting working prototype out first.
+I like the idea for jobs/building who may have multiple 
+different linked input/outputs to use recipes.
+Keep this has a second union option for JobInfo
+*/
