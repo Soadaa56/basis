8bed823982cc72db316a87ef4d5943b6881fac8e
Kevin Duffy <soadhlh@gmail.com>
2025-09-15 15:04:31 -0400

    feat: add purchaseBuilding to GameStateManager
    no longer pass buildingId as a string
    
    triggerBuilding case statements are unfinished
    
    I need logic to increment resources
    
    I'm also unsure how but I need a system for unlocking things
    
    I currently just track unlocked? and can switch booleans
    
    I need something to trigger on check or a trigger that then checks-
    
    -to decide when to visually show certain elements or pages

--- src/game/systems/BuildingSystem.ts
+++ src/game/systems/BuildingSystem.ts
@@ -1,9 +1,10 @@
 import { buildingDefinitions } from '@/game/data/buildings'
 import { BuildingTypes } from '@/game/data/buildingsInfo'
+import { JobSystem } from './JobSystem'
+import { WorkerSystem } from './WorkerSystem'
+import { ResourceSystem } from './ResourceSystem'
 
 import type { Building, BuildingId } from '@/game/models/Buildings'
-import type { ResourceSystem } from './ResourceSystem'
-import { WorkerSystem } from './WorkerSystem'
 
 export class BuildingSystem {
   private buildings: Building[] = []
@@ -28,7 +29,7 @@ export class BuildingSystem {
     })
   }
 
-  incrementBuilding(buildingId: string) {
+  incrementBuilding(buildingId: BuildingId) {
     const building = this.getBuilding(buildingId)
     if (building) {
       building.count++
@@ -41,6 +42,7 @@ export class BuildingSystem {
   triggerBuilding(
     buildingId: BuildingId,
     resourceSystem: ResourceSystem,
+    jobSystem: JobSystem,
     workerSystem: WorkerSystem,
   ) {
     const BuildingInfo = buildingDefinitions[buildingId]
@@ -66,7 +68,7 @@ export class BuildingSystem {
         const jobId = BuildingInfo.jobType
         const numberofJobs = BuildingInfo.addOpenJobs
 
-        WorkerSystem.addJobSlots(jobId, numberofJobs)
+        jobSystem.addJobSlots(jobId, numberofJobs)
         break
 
       case BuildingTypes.WorkerProducer:
@@ -76,6 +78,7 @@ export class BuildingSystem {
         break
 
       case BuildingTypes.Unlocker:
+        console.log(`triggerBuilding => case:BuildingTypes.unlocker: ${BuildingTypes.Unlocker}`)
         break
 
       case BuildingTypes.Hybrid:
--- src/game/systems/GameStateManager.ts
+++ src/game/systems/GameStateManager.ts
@@ -2,17 +2,20 @@ import { ResourceSystem } from '@/game/systems/ResourceSystem'
 import { BuildingSystem } from '@/game/systems/BuildingSystem'
 import { MagicSystem } from '@/game/systems/MagicSystem'
 import { WorkerSystem } from '@/game/systems/WorkerSystem'
+import { JobSystem } from './JobSystem'
 
 import type { Resource } from '@/game/models/Resource'
 import type { ResourceCost } from '@/game/models/Costs'
-import type { Building } from '@/game/models/Buildings'
+import type { Building, BuildingId } from '@/game/models/Buildings'
 import type { Magic } from '@/game/models/Magic'
+import type { Job } from '@/game/models/Jobs'
 
 export interface GameState {
   resources: Resource[]
   buildings: Building[]
   magic: Magic[]
   workers: number
+  jobs: Job[]
 }
 
 export class GameStateManager {
@@ -21,6 +24,7 @@ export class GameStateManager {
   buildingSystem: BuildingSystem
   magicSystem: MagicSystem
   workerSystem: WorkerSystem
+  jobSystem: JobSystem
 
   constructor(gameState: GameState) {
     this.gameState = gameState
@@ -28,11 +32,18 @@ export class GameStateManager {
     this.buildingSystem = new BuildingSystem(gameState.buildings)
     this.magicSystem = new MagicSystem(gameState.magic)
     this.workerSystem = new WorkerSystem(gameState.workers)
+    this.jobSystem = new JobSystem()
   }
 
-  purchaseBuilding(buildingId: string, buildingCosts: ResourceCost[]) {
+  purchaseBuilding(buildingId: BuildingId, buildingCosts: ResourceCost[]) {
     if (!this.resourceSystem.canAfford(buildingCosts)) return
     this.resourceSystem.spendResources(buildingCosts)
     this.buildingSystem.incrementBuilding(buildingId)
+    this.buildingSystem.triggerBuilding(
+      buildingId,
+      this.resourceSystem,
+      this.jobSystem,
+      this.workerSystem,
+    )
   }
 }
