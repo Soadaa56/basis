4a72f4c5b7b6c221d8c3012ca129cd0b1325f7dc
Kevin Duffy <soadhlh@gmail.com>
2025-09-17 17:41:35 -0400

    bug: methods correctly identify job
    Previous functions would not have worked by just
    
    passing job as JobState type

--- src/game/systems/WorkerSystem.ts
+++ src/game/systems/WorkerSystem.ts
@@ -1,9 +1,14 @@
-import type { JobState } from './JobSystem'
+import { JobSystem } from './JobSystem'
+import type { JobId } from '@/game/models/Jobs'
 
 export class WorkerSystem {
   unassignedWorkerCount: number
   maxWorkerCount: number
-  constructor(workerCount: number = 1, maxWorkerCount: number = 1) {
+  constructor(
+    private jobSystem: JobSystem,
+    workerCount: number = 1,
+    maxWorkerCount: number = 1,
+  ) {
     this.unassignedWorkerCount = workerCount
     this.maxWorkerCount = maxWorkerCount
   }
@@ -35,21 +40,27 @@ export class WorkerSystem {
     return this.unassignedWorkerCount
   }
 
-  assignWorker(job: JobState) {
+  assignWorker(jobId: JobId) {
+    const job = this.jobSystem.getJobById(jobId)
+    if (!job) return console.log(`Error at WorkerSystem: assignWorker: Job: ${job}`)
     job.assignedWorkers++
     this.decrementUnassignedWorkerCount()
   }
 
-  unassignWorker(job: JobState) {
+  unassignWorker(jobId: JobId) {
+    const job = this.jobSystem.getJobById(jobId)
+    if (!job) return console.log(`Error at WorkerSystem: unassignWorker: Job: ${job}`)
     job.assignedWorkers--
     this.incrementUnassignedWorkerCount()
   }
 
-  assignAllWorkers(job: JobState) {
+  assignAllWorkers(jobId: JobId) {
+    const job = this.jobSystem.getJobById(jobId)
+    if (!job) return console.log(`Error at WorkerSystem: assignAllWorkers: Job: ${job}`)
     const openWorkerSlots = job.MaxJobSlots - job.assignedWorkers
 
     if (this.unassignedWorkerCount >= openWorkerSlots) {
-      this.unassignedWorkerCount = -openWorkerSlots
+      this.unassignedWorkerCount -= openWorkerSlots
     } else {
       job.assignedWorkers += this.unassignedWorkerCount
       this.unassignedWorkerCount = 0
