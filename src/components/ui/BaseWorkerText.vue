46573be006a6e77e06e79dc61c34376a40f69739
Kevin Duffy <soadhlh@gmail.com>
2025-09-21 16:09:00 -0400

    feat: add baseWorkerText template

---
+++ src/components/ui/BaseWorkerText.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  name: string
+  assignedWorkers: number
+  maxJobSlots: number
+}>()
+</script>
+
+<template>
+  <div class="worker-text">
+    <span class="worker-name">{{ name }}</span>
+    <span class="worker-assigned">{{ assignedWorkers }} / {{ maxJobSlots }}</span>
+  </div>
+</template>
+
+<style scoped>
+.worker-text {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+</style>
