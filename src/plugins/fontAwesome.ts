af5ff32e7eaa8e2cac1cbf5253d5c63399c816c4
Kevin Duffy <soadhlh@gmail.com>
2025-10-05 13:13:44 -0400

    feat: import plus and minus icons

--- src/plugins/fontAwesome.ts
+++ src/plugins/fontAwesome.ts
@@ -2,11 +2,19 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { faCube, faBars, faShield, faGears, faMugSaucer } from '@fortawesome/free-solid-svg-icons'
+import {
+  faCube,
+  faBars,
+  faShield,
+  faGears,
+  faMugSaucer,
+  faPlus,
+  faMinus,
+} from '@fortawesome/free-solid-svg-icons'
 import { faWindowMaximize } from '@fortawesome/free-regular-svg-icons'
 
 // Solid
-library.add(faCube, faBars, faShield, faGears, faMugSaucer)
+library.add(faCube, faBars, faShield, faGears, faMugSaucer, faPlus, faMinus)
 // Regular
 library.add(faWindowMaximize)
 
