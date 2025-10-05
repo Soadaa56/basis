d1dddc960b4c26701c57911dd07d8cab40776f4d
Kevin Duffy <soadhlh@gmail.com>
2025-10-05 13:37:54 -0400

    feat: import gameStore into buildsingsPanel

--- src/pages/Village/components/buildings/BuildingsPanel.vue
+++ src/pages/Village/components/buildings/BuildingsPanel.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useGameStore } from '@/stores/game'
+
+const gameStore = useGameStore().manager
+</script>
 
 <template>
   <h2>Buildings Panel</h2>
