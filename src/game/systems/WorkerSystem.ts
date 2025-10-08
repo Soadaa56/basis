65d16c57844ae623d164b7cdf3096130eb6f4135
Kevin Duffy <soadhlh@gmail.com>
2025-10-08 15:48:13 -0400

    refactor: add vue reactivity to workerSystem

--- src/game/systems/WorkerSystem.ts
+++ src/game/systems/WorkerSystem.ts
@@ -7,53 +7,43 @@ export interface WorkerState {
 }
 
 export class WorkerSystem {
-  unassignedWorkerCount: number
-  maxWorkerCount: number
+  private jobSystem: JobSystem
+  private workerState: WorkerState
 
-  constructor(
-    private jobSystem: JobSystem,
-    workerState: WorkerState,
-  ) {
-    this.unassignedWorkerCount = workerState.unassignedWorkerCount
-    this.maxWorkerCount = workerState.maxWorkerCount
+  constructor(jobSystem: JobSystem, workerState: WorkerState) {
+    this.jobSystem = jobSystem
+    this.workerState = workerState
   }
 
   loadWorkers(workerState: WorkerState) {
-    this.unassignedWorkerCount = workerState.unassignedWorkerCount
-    this.maxWorkerCount = workerState.maxWorkerCount
-  }
-
-  // For Game Resets
-  resetWorkerCount(workerState: WorkerState) {
-    this.unassignedWorkerCount = workerState.unassignedWorkerCount
-    this.maxWorkerCount = workerState.maxWorkerCount
+    this.workerState = workerState
   }
 
   incrementUnassignedWorkerCount() {
-    if (this.unassignedWorkerCount < this.maxWorkerCount) {
-      this.unassignedWorkerCount++
+    if (this.workerState.unassignedWorkerCount < this.workerState.maxWorkerCount) {
+      this.workerState.unassignedWorkerCount++
     }
   }
 
   decrementUnassignedWorkerCount() {
-    if (this.unassignedWorkerCount > 0) {
-      this.unassignedWorkerCount--
+    if (this.workerState.unassignedWorkerCount > 0) {
+      this.workerState.unassignedWorkerCount--
     }
   }
 
   increaseMaxWorkerCount(newWorkers: number) {
-    this.unassignedWorkerCount += newWorkers
-    this.maxWorkerCount += newWorkers
+    this.workerState.unassignedWorkerCount += newWorkers
+    this.workerState.maxWorkerCount += newWorkers
   }
 
   getWorkerCount(): number {
-    return this.unassignedWorkerCount
+    return this.workerState.unassignedWorkerCount
   }
 
   assignWorker(jobId: JobId) {
     const job = this.jobSystem.getJobById(jobId)
     if (!job) return console.log(`Error at WorkerSystem => assignWorker => job: ${job}`)
-    if (this.unassignedWorkerCount > 0) {
+    if (this.workerState.unassignedWorkerCount > 0) {
       job.assignedWorkers++
       this.decrementUnassignedWorkerCount()
     }
@@ -73,11 +63,11 @@ export class WorkerSystem {
     if (!job) return console.log(`Error at WorkerSystem: assignAllWorkers: Job: ${job}`)
     const openWorkerSlots = job.totalJobs - job.assignedWorkers
 
-    if (this.unassignedWorkerCount >= openWorkerSlots) {
-      this.unassignedWorkerCount -= openWorkerSlots
+    if (this.workerState.unassignedWorkerCount >= openWorkerSlots) {
+      this.workerState.unassignedWorkerCount -= openWorkerSlots
     } else {
-      job.assignedWorkers += this.unassignedWorkerCount
-      this.unassignedWorkerCount = 0
+      job.assignedWorkers += this.workerState.unassignedWorkerCount
+      this.workerState.unassignedWorkerCount = 0
     }
   }
 
@@ -85,7 +75,7 @@ export class WorkerSystem {
     const job = this.jobSystem.getJobById(jobId)
     if (!job) return console.log(`Error at WorkerSystem: unassignAllWorkers: Job: ${job}`)
 
-    this.unassignedWorkerCount += job.assignedWorkers
+    this.workerState.unassignedWorkerCount += job.assignedWorkers
     job.assignedWorkers = 0
   }
 }
