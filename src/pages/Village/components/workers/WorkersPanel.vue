cda7cb15c591d375895b432c086e6c4dc407a53b
Kevin Duffy <soadhlh@gmail.com>
2025-10-08 16:29:40 -0400

    feat: add reactivity to worker panel numbers

--- src/pages/Village/components/workers/WorkersPanel.vue
+++ src/pages/Village/components/workers/WorkersPanel.vue
@@ -2,25 +2,25 @@
 import { useGameStore } from '@/stores/game'
 import BaseWorkerText from '@/components/ui/BaseWorkerText.vue'
 
-const gameStore = useGameStore().manager
+const gameStore = useGameStore()
 </script>
 
 <template>
   <div v-if="gameStore" class="worker-panel-container">
     <h2>Workers Panel</h2>
     <h4>
-      {{ gameStore?.gameState.workers.unassignedWorkerCount }} /
-      {{ gameStore?.gameState.workers.maxWorkerCount }} Worker Unassigned
+      {{ gameStore.manager.gameState.workers.unassignedWorkerCount }} /
+      {{ gameStore.manager.gameState.workers.maxWorkerCount }} Worker Unassigned
     </h4>
     <base-worker-text
-      v-for="job in gameStore.gameState.jobs"
+      v-for="job in gameStore.manager.gameState.jobs"
       :key="job.id"
       :id="job.id"
       :name="job.name"
       :assigned-workers="job?.assignedWorkers"
       :max-job-slots="job?.totalJobs"
-      @assign="gameStore.addWorkerToJob(job.id)"
-      @unassign="gameStore.removeWorkerFromJob(job.id)"
+      @assign="gameStore.manager.addWorkerToJob(job.id)"
+      @unassign="gameStore.manager.removeWorkerFromJob(job.id)"
     />
   </div>
 </template>
