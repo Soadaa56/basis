e775082359e2534b75a9fd795382bc04bc8bf091
Kevin Duffy <soadhlh@gmail.com>
2025-10-08 12:01:06 -0400

    feat: add gameSTateManager to window for debugging

--- src/game/bootstrap.ts
+++ src/game/bootstrap.ts
@@ -4,3 +4,8 @@ import { GameStateManager } from '@/game/systems/GameStateManager'
 const saveFile = loadSaveFile()
 
 export const gameStateManager = saveFile ? new GameStateManager(saveFile.gameState) : null
+
+if (import.meta.env.DEV) {
+  // @ts-expect-error: for debugging only
+  window.gameManager = gameStateManager
+}
