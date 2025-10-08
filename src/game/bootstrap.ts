953cf55f5be02de7e7b69e49c8c470ea26e0ffcd
Kevin Duffy <soadhlh@gmail.com>
2025-10-08 16:11:40 -0400

    minor: ensure gameStateManger is not null

--- src/game/bootstrap.ts
+++ src/game/bootstrap.ts
@@ -1,9 +1,12 @@
 import { loadSaveFile } from '@/utils/saveFile'
 import { GameStateManager } from '@/game/systems/GameStateManager'
+import { initialGameState } from './data/initialGameState'
 
 const saveFile = loadSaveFile()
 
-export const gameStateManager = saveFile ? new GameStateManager(saveFile.gameState) : null
+export const gameStateManager = new GameStateManager(
+  saveFile ? saveFile.gameState : initialGameState,
+)
 
 if (import.meta.env.DEV) {
   // @ts-expect-error: for debugging only
