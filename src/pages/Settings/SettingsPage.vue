b5d55587f6afda952d2829ca5fb759897574b467
Kevin Duffy <soadhlh@gmail.com>
2025-10-11 22:55:09 -0400

    minor: reset game data button for dev

--- src/pages/Settings/SettingsPage.vue
+++ src/pages/Settings/SettingsPage.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
 import FooterLayout from '@/components/layouts/FooterLayout.vue'
+
+// Fast easy way to reset, will be moved and add a confirm
+function resetGameData() {
+  localStorage.removeItem('saveFile')
+  window.location.reload()
+}
 </script>
 
 <template>
   <h1>Settings</h1>
+
+  <button @click="resetGameData">Reset Game Data</button>
+
   <footer-layout />
 </template>
 
-<style scoped></style>
+<style scoped>
+button {
+  margin: 16px;
+  padding: 16px;
+  border: 4px dashed darkred;
+}
+</style>
