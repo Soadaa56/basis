13e9170bca6cffa9998389e3224d9a81d723efa0
Kevin Duffy <soadhlh@gmail.com>
2025-10-11 20:15:59 -0400

    feat: simplify jobInfo
    Focus on prototype before complexity

--- src/game/data/jobsInfo.ts
+++ src/game/data/jobsInfo.ts
@@ -13,25 +13,8 @@ export interface JobInput {
 }
 
 // Not sure if any job will do anything besides produce resources
-export type JobInfo =
-  | {
-      unlocked: boolean
-      output: JobOutput[]
-      input?: JobInput[]
-    }
-  | {
-      unlocked: boolean
-      recipes: [
-        {
-          unlocked: boolean
-          output: JobOutput[]
-          input: JobInput[]
-        },
-      ]
-    }
-
-/* I'm focusing on getting working prototype out first.
-I like the idea for jobs/building who may have multiple 
-different linked input/outputs to use recipes.
-Keep this has a second union option for JobInfo
-*/
+export type JobInfo = {
+  unlocked: boolean
+  output: JobOutput[]
+  input?: JobInput[]
+}
