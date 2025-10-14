243f075167a4b4cbaf4b0e1d4985fca2341b1b59
Kevin Duffy <soadhlh@gmail.com>
2025-10-14 13:18:11 -0400

    minor: incomplete building card template for panel

--- src/pages/Village/components/buildings/BuildingsPanel.vue
+++ src/pages/Village/components/buildings/BuildingsPanel.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
 import { useGameStore } from '@/stores/game'
+import BaseBuildingCard from '@/components/ui/BaseBuildingCard.vue'
 
-const gameStore = useGameStore().manager
+const gameStore = useGameStore()
 </script>
 
 <template>
   <h2>Buildings Panel</h2>
+  <BaseBuildingCard
+    v-for="building in gameStore.manager.gameState.buildings"
+    :key="building.id"
+    :id="building.id"
+    :name="building.name"
+    :cost="building.cost"
+    :count="building.count"
+    :purchase-effect-text="building.purchaseEffectText"
+    :flavor-text="building.flavorText"
+  />
 </template>
 
 <style scoped></style>
