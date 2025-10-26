580c63a83b5a8286571e05f4af880897954f3f1d
Kevin Duffy <soadhlh@gmail.com>
2025-10-26 19:47:40 -0400

    chore: add floating point fix

--- src/components/ui/BaseResourceText.vue
+++ src/components/ui/BaseResourceText.vue
@@ -1,4 +1,21 @@
 <script setup lang="ts">
+function formatIncome(income: number) {
+  if (income == 0) {
+    return 0
+  }
+  if (income < 0.01) {
+    return income.toFixed(3)
+  }
+  if (income < 1) {
+    return income.toFixed(2)
+  }
+  if (income < 100) {
+    return income.toFixed(1)
+  }
+
+  return Math.round(income).toString
+}
+
 defineProps<{
   name: string
   value: number
@@ -10,8 +27,8 @@ defineProps<{
 <template>
   <div class="resource-text">
     <span class="resource-name">{{ name }}:</span>
-    <span class="resource-numbers">{{ value.toFixed(1) }}/{{ max }}</span>
-    <span class="income">(+{{ income }}/s)</span>
+    <span class="resource-numbers">{{ value.toFixed(0) }}/{{ max }}</span>
+    <span class="income">(+{{ formatIncome(income) }}/s)</span>
   </div>
 </template>
 
