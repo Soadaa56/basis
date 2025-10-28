97cb863194087daf4f781c23bac357fe07cbf299
Kevin Duffy <soadhlh@gmail.com>
2025-10-28 16:41:55 -0400

    feat: add job descriptions record

---
+++ src/game/models/jobs/jobDescriptions.ts
@@ -0,0 +1,12 @@
+import type { JobId } from '../Jobs'
+
+export const jobDescription: Partial<Record<JobId, string>> = {
+  baker: 'Bakes fresh bread and pastries.',
+  beggar: 'Goes to the city to beg for coin. Probably shares most of their earnings with you',
+  blacksmith: 'Turns useless lumps of metal into tools.',
+  farmer: 'Works the field.',
+  fishermen: "Pretends they're hard at work most of the day.",
+  lumberjack: "Sterotypical 'chad' job.",
+  merchant: 'What are ya selling, stranger?',
+  miner: "*cough**cough* I think I'm getting the black lung pop.",
+}
