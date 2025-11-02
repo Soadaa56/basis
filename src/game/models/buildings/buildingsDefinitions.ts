9c389a4eb47b99293434ba042ee3a9ebe9c454c2
Kevin Duffy <soadhlh@gmail.com>
2025-11-02 14:47:07 -0500

    feat: add unlockable to buildingDefinitions

--- src/game/models/buildings/buildingsDefinitions.ts
+++ src/game/models/buildings/buildingsDefinitions.ts
@@ -1,8 +1,9 @@
 import type { BuildingId } from '@/game/data/buildingsId'
 import type { BuildingInfo } from '@/game/models/buildings/buildingsInfo'
 import type { ResourceCost } from '@/game/models/Resource'
+import type { Unlockable } from '../Unlockable'
 
-export interface BuildingDefinition {
+export interface BuildingDefinition extends Unlockable {
   id: BuildingId
   name: string
   cost: ResourceCost[]
