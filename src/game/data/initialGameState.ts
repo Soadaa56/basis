b253165c31d33e5bf528cd999617fb38c694476f
Kevin Duffy <soadhlh@gmail.com>
2025-11-09 21:07:21 -0500

    chore: split research into unlocked and completed

--- src/game/data/initialGameState.ts
+++ src/game/data/initialGameState.ts
@@ -107,5 +107,8 @@ export const initialGameState: GameState = {
   jobs: initialJobs,
   magic: [],
   workers: initialWorkers,
-  researches: [],
+  researches: {
+    unlocked: [],
+    completed: [],
+  },
 }
