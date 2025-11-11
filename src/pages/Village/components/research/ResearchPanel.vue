12fb0dcabd1407bb004bf7290c5d0764b0557bd4
Kevin Duffy <soadhlh@gmail.com>
2025-11-10 20:30:02 -0500

    feat: import baseResearchCard component
    pass variables; cleanup to pass only required variables
    
    Avoid prop drilling variables to future implementation of research tooltip

--- src/pages/Village/components/research/ResearchPanel.vue
+++ src/pages/Village/components/research/ResearchPanel.vue
@@ -1,18 +1,33 @@
 <script setup lang="ts">
 import { useGameStore } from '@/stores/game'
+import BaseResearchCard from '@/components/ui/BaseResearchCard.vue'
 
 const gameStore = useGameStore()
 </script>
 
 <template>
   <h1>Research</h1>
-  <div class="research-panel"></div>
+  <div class="research-panel">
+    <BaseResearchCard
+      v-for="research in gameStore.manager.gameState.researches"
+      :key="research.id"
+      :id="research.id"
+      :name="research.name"
+      :tier="research.tier"
+      :cost="research.cost"
+      :effect="research.effect"
+      :state="research.state"
+      :category="research.category"
+      :description="research.description"
+      :resource-cost="research.resourceCost"
+    />
+  </div>
 </template>
 
 <style scoped lang="scss">
 .research-panel {
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: repeat(4, 1fr);
   gap: 1rem;
 }
 </style>
