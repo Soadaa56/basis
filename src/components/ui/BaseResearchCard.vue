8a851c2d4f3a596457906589a5e9d07c7a2b93ac
Kevin Duffy <soadhlh@gmail.com>
2025-11-10 20:26:16 -0500

    wip: add baseResearchCard starting point

---
+++ src/components/ui/BaseResearchCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import type { ResearchCategory, ResearchEffect, Tier } from '@/game/models/Research'
+import type { ResearchState } from '@/game/models/researches/ResearchState'
+import type { ResourceCost } from '@/game/models/Resource'
+// import { useGameStore } from '@/stores/game'
+
+// const gameStore = useGameStore()
+
+defineProps<{
+  id: string
+  name: string
+  tier: Tier
+  cost: number
+  effect: ResearchEffect[]
+  state?: ResearchState
+  category?: ResearchCategory
+  description?: string
+  resourceCost?: ResourceCost
+}>()
+
+const emit = defineEmits<{
+  (e: 'purchase', researchId: string): void
+}>()
+</script>
+
+<template>
+  <div class="research-card" @click="emit('purchase', id)">
+    <p class="research-name">{{ name }}</p>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
