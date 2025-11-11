7c4031c917c712065939fa28aa8b25ffd365e52b
Kevin Duffy <soadhlh@gmail.com>
2025-11-10 20:14:10 -0500

    feat: add base research panel page

---
+++ src/pages/Village/components/research/ResearchPanel.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useGameStore } from '@/stores/game'
+
+const gameStore = useGameStore()
+</script>
+
+<template>
+  <h1>Research</h1>
+  <div class="research-panel"></div>
+</template>
+
+<style scoped lang="scss">
+.research-panel {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 1rem;
+}
+</style>
