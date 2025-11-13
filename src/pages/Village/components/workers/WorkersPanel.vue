b0108c2741c467711ec7f2aaf5533d3858c91f57
Kevin Duffy <soadhlh@gmail.com>
2025-11-13 18:14:51 -0500

    style: color for workers without job limit

--- src/pages/Village/components/workers/WorkersPanel.vue
+++ src/pages/Village/components/workers/WorkersPanel.vue
@@ -3,6 +3,9 @@ import BaseWorkerText from '@/components/ui/BaseWorkerText.vue'
 import { useGameStore } from '@/stores/game'
 
 const gameStore = useGameStore()
+
+const jobLimited = gameStore.manager.gameState.jobs.filter((job) => job.totalJobs !== Infinity)
+const jobUnlimited = gameStore.manager.gameState.jobs.filter((job) => job.totalJobs === Infinity)
 </script>
 <template>
   <div v-if="gameStore" class="worker-panel-container">
@@ -12,7 +15,7 @@ const gameStore = useGameStore()
       {{ gameStore.manager.gameState.workers.maxWorkerCount }} Worker Unassigned
     </h3>
     <BaseWorkerText
-      v-for="job in gameStore.manager.gameState.jobs"
+      v-for="job in jobUnlimited"
       :key="job.id"
       :jobId="job.id"
       :name="job.name"
@@ -20,8 +23,28 @@ const gameStore = useGameStore()
       :max-job-slots="job?.totalJobs"
       @assign="gameStore.manager.addWorkerToJob(job.id)"
       @unassign="gameStore.manager.removeWorkerFromJob(job.id)"
+      class="job-unlimited"
+    />
+    <BaseWorkerText
+      v-for="job in jobLimited"
+      :key="job.id"
+      :jobId="job.id"
+      :name="job.name"
+      :assigned-workers="job?.assignedWorkers"
+      :max-job-slots="job?.totalJobs"
+      @assign="gameStore.manager.addWorkerToJob(job.id)"
+      @unassign="gameStore.manager.removeWorkerFromJob(job.id)"
+      class="job-limited"
     />
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.job-unlimited {
+  color: var(--accent-color);
+}
+
+.job-limited {
+  color: var(--secondary-color);
+}
+</style>
