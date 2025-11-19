ddc927cf4ed9f86930f4129565970239050db8ed
Kevin Duffy <soadhlh@gmail.com>
2025-11-19 16:24:19 -0500

    feat: add save game button

--- src/pages/Settings/SettingsPage.vue
+++ src/pages/Settings/SettingsPage.vue
@@ -1,17 +1,42 @@
 <script setup lang="ts">
 import FooterLayout from '@/components/layouts/FooterLayout.vue'
+import { saveGameData, type GameData } from '@/utils/saveFile'
+import { useGameStore } from '@/stores/game'
+import router from '@/router'
 
-// Fast easy way to reset, will be moved and add a confirm
-function resetGameData() {
-  localStorage.removeItem('saveFile')
-  window.location.reload()
+const gameStore = useGameStore()
+
+// Fast easy way to reset. Will be moved and double confirm to activate.
+function resetGameDataButton(): void {
+  localStorage.removeItem('gameData')
+  router.push('/welcome')
+}
+
+function saveGameDataButton(): void {
+  const rawData = localStorage.getItem('gameData')
+  const jsonGameData = rawData ? JSON.parse(rawData) : {}
+  const gameData: GameData = {
+    version: '0.0.0-test',
+    createdAt: jsonGameData.createdAt || Date.now(),
+    updatedAt: Date.now(),
+    villageName: jsonGameData.villageName || 'Basis Village',
+    gameState: gameStore.manager.gameState,
+  }
+
+  console.log(
+    `Game data saved. Version ${gameData.version} for village ${gameData.villageName} at time ${gameData.updatedAt}.`,
+  )
+  saveGameData(gameData)
+  router.push('/')
 }
 </script>
 
 <template>
   <h1>Settings</h1>
 
-  <button id="reset-button" @click="resetGameData">Reset Game Data</button>
+  <button id="save-button" @click="saveGameDataButton">Save Game</button>
+
+  <button id="reset-button" @click="resetGameDataButton">Reset Game Data</button>
 
   <footer-layout />
 </template>
