967b122a62edda908037e74b241f46974ade7ae5
Kevin Duffy <soadhlh@gmail.com>
2025-11-19 16:35:54 -0500

    fix portfolio link

--- src/components/layouts/FooterLayout.vue
+++ src/components/layouts/FooterLayout.vue
@@ -4,7 +4,7 @@
   <footer>
     <div id="footer-copyright">
       <p>Created by Kevin Duffy - © 2025</p>
-      <p><a href="https://kevin.duffy.dev">KevinDuffy.dev</a></p>
+      <p><a href="https://kevinduffy.dev">KevinDuffy.dev</a></p>
     </div>
   </footer>
 </template>
