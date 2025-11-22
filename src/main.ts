6d602872b22029222ab1e64425ff698d8eaaee3b
Kevin Duffy <soadhlh@gmail.com>
2025-11-22 15:36:06 -0500

    remove gameRestore logic out of main

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import { restoreGameState } from './utils/restoreGameState'
 
 // Custom plugins
 import FontAwesomeIcon from './plugins/fontAwesome'
@@ -16,12 +15,5 @@ app.component('FaIcon', FontAwesomeIcon)
 
 app.use(createPinia())
 app.use(router)
-
-// Restore gameState if found
-const gameData = localStorage.getItem('gameData')
-if (gameData) {
-  const parsed = JSON.parse(gameData)
-  restoreGameState(parsed.gameState)
-}
 
 app.mount('#app')
